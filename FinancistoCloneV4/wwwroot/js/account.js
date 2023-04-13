9d212075ba14084c3ab6769b987c3ae0ff695fab
Jhan242019 <jahn24@hotmail.com>
2023-04-12 20:56:50 -0500

    Add SweetAlert2 Delete, Login

--- FinancistoCloneV4/wwwroot/js/account.js
+++ FinancistoCloneV4/wwwroot/js/account.js
@@ -1,18 +1,41 @@
-﻿var datatable;
-
-$(document).ready(function () {
-    loadDataTable();
-});
-
-function loadDataTable() {
-    datatable = $('#tablaData').DataTable({
-        "ajax": {
-            "url":"/Account/ListaData"
-        },
-        "columns": [
-            { "data": "name", "width": "20%"},
-            { "data": "currency", "width": "20%" },
-            { "data": "amount", "width": "20%" },
-        ]
-    });
+﻿function deleteAccount(id) {
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¡Esta cuenta se eliminará de tus cuentas!",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: '<i class="fas fa-times-circle"></i> Cancelar',
+        confirmButtonText: '<i class="fas fa-check-circle"></i> Sí, Eliminar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: "/account/delete?id=" + id,
+                type: "get"
+            });
+            Swal.fire({
+                title: 'Eliminado!',
+                text: 'Esta cuenta se eliminó de tus cuentas',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    location.reload();
+                }
+            })
+        }
+    })
 }
+
+function loginCorrect() {
+    Swal.fire({
+        title: '¡Bienvenido!',
+        text: 'Inicio de sesión exitoso',
+        icon: 'success',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Aceptar'
+    })
+}
+
+
