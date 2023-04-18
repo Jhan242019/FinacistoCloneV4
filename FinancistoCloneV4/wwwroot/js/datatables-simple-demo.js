e4055ee30cb1e986f6320605efe170df39c93fd9
Jhan242019 <jahn24@hotmail.com>
2023-04-18 00:45:26 -0500

    Update Layout

--- FinancistoCloneV4/wwwroot/js/datatables-simple-demo.js
+++ FinancistoCloneV4/wwwroot/js/datatables-simple-demo.js
@@ -5,5 +5,5 @@ window.addEventListener('DOMContentLoaded', event => {
     const datatablesSimple = document.getElementById('datatablesSimple');
     if (datatablesSimple) {
         new simpleDatatables.DataTable(datatablesSimple);
-    }
+    }    
 });
